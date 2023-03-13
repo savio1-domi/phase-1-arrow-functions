47a2bbbc5e4a37dcabfb7d302fad69e6a5dcf4d5
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-13 18:28:20 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+// creating a function expression called divide and 
+// divides 2000 by 100   
 const divide = (num1, num2) => 2000/100;
+
+// creating an arrow function called square
+// it takes one parameter and multiples it by itself
 const square =  num => num*num;
+
+// creating an arrow function called add
+// it takes two parameters and adds them together 
 const add = (num1, num2) => num1 + num2;
- 
